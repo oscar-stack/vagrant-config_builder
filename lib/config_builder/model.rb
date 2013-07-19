68556d7a2ba5c1e2af44968e18c97b30544c7727
Adrien Thebo <git@somethingsinistral.net>
2013-07-19 15:31:22 -0700

    Require model components in top level model class

--- lib/config_builder/model.rb
+++ lib/config_builder/model.rb
@@ -1,5 +1,14 @@
 module ConfigBuilder
   module Model
-    require 'config_builder/model/node'
+    require 'config_builder/model/synced_folder'
+
+    module Network
+      require 'config_builder/model/network/forwarded_port'
+      require 'config_builder/model/network/private_network'
+    end
+
+    module Provider
+      require 'config_builder/model/provider/virtualbox'
+    end
   end
 end
