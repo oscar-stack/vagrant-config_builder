5f8b05f15fd33a3edb71c24343d6652624290652
Adrien Thebo <git@somethingsinistral.net>
2013-07-19 15:18:48 -0700

    Add network/forwarded_port model

---
+++ lib/config_builder/model/network/forwarded_port.rb
@@ -0,0 +1,21 @@
+require 'activemodel'
+
+# Vagrant forwarded port model
+#
+# @see http://docs.vagrantup.com/v2/networking/forwarded_ports.html
+class ConfigBuilder::Model::Network::ForwardedPort
+
+  # @!attribute [rw] guest
+  #   @return [Fixnum] The guest port
+  attr_accessor :guest
+
+  # @!attribute [rw] host
+  #   @return [Fixnum] The host port
+  attr_accessor :host
+
+  # @!attribute [rw] auto_correct
+  #   @return [Boolean] Whether to automatically correct port collisions
+  attr_accessor :auto_correct
+
+
+end
