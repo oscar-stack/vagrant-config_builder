822d8c7bf7c71a87e2e0610f6f1b70f18b381082
Adrien Thebo <git@somethingsinistral.net>
2013-07-19 16:28:05 -0700

    Add basic vm model

--- lib/config_builder/model.rb
+++ lib/config_builder/model.rb
@@ -1,6 +1,7 @@
 module ConfigBuilder
   module Model
     require 'config_builder/model/synced_folder'
+    require 'config_builder/model/vm'
 
     module Network
       require 'config_builder/model/network/forwarded_port'
---
+++ lib/config_builder/model/vm.rb
@@ -0,0 +1,26 @@
+require 'activemodel'
+
+class ConfigBuilder::Model::VM
+
+  # @!attribute [rw] provider
+  #   @return [ConfigBuilder::Model::Provider] The provider configuration for
+  #     this VM
+  attr_accessor :provider
+
+  # @!attribute [rw] provisioners
+  #   @return [Array<ConfigBuilder::Model::Provisioner>] A list of provisioner
+  #     configurations for this VM
+  attr_accessor :provisioners
+
+  # @!attribute [rw] forwarded_ports
+  #   @return [Array<ConfigBuilder::Model::Network::ForwardedPort>]
+  attr_accessor :forwarded_ports
+
+  # @!attribute [rw] private_networks
+  #   @return [Array<ConfigBuilder::Model::Network::PrivateNetwork>]
+  attr_accessor :private_networks
+
+  # @!attribute [rw] synced_folders
+  #   @return [Array<ConfigBuilder::Model::SyncedFolder>]
+  attr_accessor :synced_folders
+end
