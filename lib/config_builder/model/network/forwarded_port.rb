9f231eeb970f7a558243ddaaf46d8c53475c2e5d
Adrien Thebo <git@somethingsinistral.net>
2013-07-23 16:35:30 -0700

    Implement Model::Network::ForwardedPort#to_proc

--- lib/config_builder/model/network/forwarded_port.rb
+++ lib/config_builder/model/network/forwarded_port.rb
@@ -1,7 +1,7 @@
 # Vagrant forwarded port model
 #
 # @see http://docs.vagrantup.com/v2/networking/forwarded_ports.html
-class ConfigBuilder::Model::Network::ForwardedPort
+class ConfigBuilder::Model::Network::ForwardedPort < ConfigBuilder::Model
 
   # @!attribute [rw] guest
   #   @return [Fixnum] The guest port
@@ -14,4 +14,14 @@ class ConfigBuilder::Model::Network::ForwardedPort
   # @!attribute [rw] auto_correct
   #   @return [Boolean] Whether to automatically correct port collisions
   attr_accessor :auto_correct
+
+  def initialize
+    @auto_correct = false
+  end
+
+  def to_proc
+    Proc.new do |config|
+      config.network(:forwarded_port, :guest => @guest, :host => @host, :auto_correct => @auto_correct)
+    end
+  end
 end
