75ed63e8a1f4f4c2333380fcd2a4d758e61a0c90
Adrien Thebo <git@somethingsinistral.net>
2013-07-24 14:42:57 -0700

    Add example documentation for Model::VM

--- lib/config_builder/model/vm.rb
+++ lib/config_builder/model/vm.rb
@@ -2,24 +2,61 @@
 class ConfigBuilder::Model::VM
 
   # @!attribute [rw] provider
-  #   @return [ConfigBuilder::Model::Provider] The provider configuration for
+  #   @return [Hash<Symbol, Object>] The provider configuration for
   #     this VM
+  #   @example
+  #     >> vm.provider
+  #     => {
+  #           :type => 'virtualbox',
+  #           :name => 'tiny-tina',
+  #           :gui  => false,
+  #        }
   attr_accessor :provider
 
   # @!attribute [rw] provisioners
-  #   @return [Array<ConfigBuilder::Model::Provisioner>] A list of provisioner
-  #     configurations for this VM
+  #   @return [Array<Hash<Symbol, Object>>] A collection of provisioner
+  #     parameters in the order that they should be applied
+  #     of provisioner types, and a list of provisioner instances for each type
+  #   @example
+  #     >> vm.provisioners
+  #     => [
+  #           {:type => :shell, :path   => '/vagrant/bin/magic.sh'},
+  #           {:type => :shell, :inline => '/bin/echo hello world'},
+  #
+  #           {:type => :puppet, :manifest => 'foo.pp'},
+  #           {:type => :puppet, :manifest => 'bar.pp', :modulepath => '/vagrant/modules'},
+  #        ]
   attr_accessor :provisioners
 
   # @!attribute [rw] forwarded_ports
-  #   @return [Array<ConfigBuilder::Model::Network::ForwardedPort>]
+  #   @return [Array<Hash<Symbol, Object>>] A collection of port mappings
+  #   @example
+  #     >> vm.forwarded_ports
+  #     => [
+  #           {:guest => 80, :host  => 20080},
+  #           {:guest => 443, :host => 20443},
+  #        ]
   attr_accessor :forwarded_ports
 
   # @!attribute [rw] private_networks
-  #   @return [Array<ConfigBuilder::Model::Network::PrivateNetwork>]
+  #   @return [Array<Hash<Symbol, Object>>] A collection of IP address network
+  #     settings.
+  #   @example
+  #     >> vm.private_networks
+  #     => [
+  #           {:ip => '10.20.4.1'},
+  #           {:ip => '192.168.100.5', :netmask => '255.255.255.128'},
+  #        ]
   attr_accessor :private_networks
 
   # @!attribute [rw] synced_folders
-  #   @return [Array<ConfigBuilder::Model::SyncedFolder>]
+  #   @return [Array<Hash<Symbol, Object>>]
+  #   @example
+  #     >> vm.synced_folders
+  #     => [
+  #           {:host_path => 'manifests/', :guest_path => '/root/manifests', :disabled => false},
+  #           {:host_path => 'modules/', :guest_path => '/root/modules'},
+  #        ]
+  #
   attr_accessor :synced_folders
 end
