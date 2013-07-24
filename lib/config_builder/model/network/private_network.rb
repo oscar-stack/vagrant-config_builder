1220d4d1c9b66afff4ec6325f0d89cc021a4c645
Adrien Thebo <git@somethingsinistral.net>
2013-07-23 23:43:58 -0700

    Implement Network::PrivateNetwork#to_proc

--- lib/config_builder/model/network/private_network.rb
+++ lib/config_builder/model/network/private_network.rb
@@ -1,9 +1,15 @@
 # Vagrant private network
 #
 # @see http://docs.vagrantup.com/v2/networking/private_network.html
-class ConfigBuilder::Model::Network::PrivateNetwork
+class ConfigBuilder::Model::Network::PrivateNetwork < ConfigBuilder::Model
 
   # @!attribute [rw] :ip
   #   @return [String] The IP address to use for the private network interface
   attr_accessor :ip
+
+  def to_proc
+    Proc.new do |config|
+      config.network(:private_network, :ip => @ip)
+    end
+  end
 end
