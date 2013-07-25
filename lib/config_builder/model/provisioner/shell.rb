7d4c6053325bd65854de9b3f78b860343c24d493
Adrien Thebo <git@somethingsinistral.net>
2013-07-24 17:05:53 -0700

    Implement Model::Provisioner::Shell#to_proc

--- lib/config_builder/model/provisioner/shell.rb
+++ lib/config_builder/model/provisioner/shell.rb
@@ -1,5 +1,5 @@
 # @see http://docs.vagrantup.com/v2/provisioning/shell.html
-class ConfigBuilder::Model::Provisioner::Shell
+class ConfigBuilder::Model::Provisioner::Shell < ConfigBuilder::Model
 
   # @!attribute [rw] inline
   #   @return [String] The inline shell command to run
@@ -13,4 +13,13 @@ class ConfigBuilder::Model::Provisioner::Shell
   #   @return [String] A string acting as an argument vector to the command.
   attr_accessor :args
 
+  def to_proc
+    Proc.new do |vm_config|
+      vm_config.provision :shell do |shell_config|
+        shell_config.inline = @inline if defined? @inline
+        shell_config.path   = @path   if defined? @path
+        shell_config.args   = @args   if defined? @args
+      end
+    end
+  end
 end
