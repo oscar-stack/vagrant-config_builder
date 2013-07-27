6eb5f3112633503b4d8471cd0f6315de851f88bd
Adrien Thebo <git@somethingsinistral.net>
2013-07-26 18:44:38 -0700

    Skeleton implementation of Model::Root#to_proc

--- lib/config_builder/model/root.rb
+++ lib/config_builder/model/root.rb
@@ -5,4 +5,29 @@ class ConfigBuilder::Model::Root < ConfigBuilder::Model::Base
   attr_accessor :host
   attr_accessor :ssh
   attr_accessor :vms
+
+  def to_proc
+    Proc.new do |root_config|
+      eval_host(root_config)
+      eval_ssh(root_config)
+      eval_vms(root_config)
+    end
+  end
+
+  private
+
+  def eval_vms(root_config)
+    @vms.each do |hash|
+      v = ConfigBuilder::Model::VM.new_from_hash(hash)
+      v.call(root_config)
+    end
+  end
+
+  def eval_host(root_config)
+
+  end
+
+  def eval_ssh(root_config)
+
+  end
 end
