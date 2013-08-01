c9912d85c14182e57c29febe3c5ac182e7387ddc
Adrien Thebo <git@somethingsinistral.net>
2013-08-01 15:04:24 -0700

    Update synced_folder to use new attribute syntax

--- lib/config_builder/model/synced_folder.rb
+++ lib/config_builder/model/synced_folder.rb
@@ -5,28 +5,28 @@ class ConfigBuilder::Model::SyncedFolder < ConfigBuilder::Model::Base
 
   # @!attribute [rw] host_path
   #   @return [String] The host file path to mount on the guest
-  attr_accessor :host_path
+  def_model_attribute :host_path
 
   # @!attribute [rw] guest_path
   #   @return [String] The guest file path to be used as the mount point
-  attr_accessor :guest_path
+  def_model_attribute :guest_path
 
   # @!attribute [rw] extra
   #   A set of arbitrary options to pass to the virtualbox mount command.
   #   @return [String]
-  attr_accessor :extra
+  def_model_attribute :extra
 
   # @!attribute [rw] disabled
   #   @return [Boolean] If the mount point should be disabled.
-  attr_accessor :disabled
+  def_model_attribute :disabled
 
   # @!attribute [rw] nfs
   #   @return [Boolean] If the mount point should use NFS
-  attr_accessor :nfs
+  def_model_attribute :nfs
 
   def to_proc
     Proc.new do |vm_config|
-      vm_config.synced_folder(@host_path, @guest_path, folder_opts)
+      vm_config.synced_folder(attr(:host_path), attr(:guest_path), folder_opts)
     end
   end
 
@@ -34,9 +34,9 @@ class ConfigBuilder::Model::SyncedFolder < ConfigBuilder::Model::Base
 
   def folder_opts
     h = {}
-    h[:extra]    = @extra    if defined? @extra
-    h[:disabled] = @disabled if defined? @disabled
-    h[:nfs]      = @nfs      if defined? @nfs
+    h[:extra]    = attr(:extra)    if attr(:extra)
+    h[:disabled] = attr(:disabled) if attr(:disabled)
+    h[:nfs]      = attr(:nfs)      if attr(:nfs)
 
     h
   end
