d79110e1b6cbc167e4ab783f463365d9f7ae594d
Adrien Thebo <git@somethingsinistral.net>
2013-08-01 15:56:20 -0700

    Add basic ConfigBuilder.load method
    It's a trivial implementation to get an MVP out of the door.

--- lib/config_builder.rb
+++ lib/config_builder.rb
@@ -5,4 +5,17 @@ module ConfigBuilder
   require 'config_builder/model'
   require 'config_builder/plugin'
   require 'config_builder/version'
+
+  def self.load(identifier, method, value)
+    if identifier != :yaml
+      raise NotImplementedError, "Multiple loader backends are not implemented"
+    end
+
+    loader = ConfigBuilder::Loader::YAML.new
+    data   = loader.send(method, value)
+    model  = ConfigBuilder::Model::Root.new_from_hash(data)
+    require 'pry'
+    binding.pry
+    model.to_proc
+  end
 end
