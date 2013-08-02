94fe66324778a1e4a5cfb47910ba4a676e283ef5
Adrien Thebo <git@somethingsinistral.net>
2013-08-02 11:42:59 -0700

    Remove hardcoded filter stack setting
    ...with a slightly less hardcoded filter stack setting.

--- lib/config_builder/filter_stack.rb
+++ lib/config_builder/filter_stack.rb
@@ -5,15 +5,26 @@ module ConfigBuilder
 
     def initialize
       @filter_stack = []
-      @filter_stack << ConfigBuilder::Filter::Roles.new
     end
 
     # @param input [Hash]
     #
     # @return [Hash]
     def filter(input)
+      set_filters(input.delete(:filters))
+
       @filter_stack.inject(input) do |current_input, filter|
         filter.run(current_input)
+      end
+    end
+
+    private
+
+    def set_filters(list)
+      if list.nil?
+        @filter_stack << ConfigBuilder::Filter::Roles.new
+      else
+        raise NotImplementedError, "Custom lists of filters not implemented."
       end
     end
   end
