3d9e5d54ee3c82369704f458ba1012a9c76db9a2
Adrien Thebo <git@somethingsinistral.net>
2013-08-18 19:05:05 -0700

    [class_registry] Fix DuplicateEntry error message, take 2

--- lib/config_builder/class_registry.rb
+++ lib/config_builder/class_registry.rb
@@ -29,8 +29,7 @@ module ConfigBuilder
     def register(identifier, klass)
       if @klasses[identifier]
         raise DuplicateEntry, :registry => @name,
-                              :identifier => identifier.inspect,
-                              :identifiers => @klasses.keys
+                              :identifier => identifier.inspect
       else
         @klasses[identifier] = klass
       end
