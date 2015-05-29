85058a392756966ef7578f8d0dc43d73756ee7a0
llowder <lee@puppetlabs.com>
2015-05-29 14:04:33 -0500

    This commit adds support for run options to the puppet_server provisioner

--- lib/config_builder/model/provisioner/puppet_server.rb
+++ lib/config_builder/model/provisioner/puppet_server.rb
@@ -13,9 +13,19 @@ class ConfigBuilder::Model::Provisioner::PuppetServer < ConfigBuilder::Model::Ba
   #   @return [String]
   attr_accessor :options
 
+  # @!attribute [rw] run
+  #   @return [String] Defaults to not set. If set to 'always' will cause provisioner to always run.
+  attr_accessor :run
+
+  def initialize
+    @defaults = {
+     :run => 'once',
+    }
+  end
+
   def to_proc
     Proc.new do |vm_config|
-      vm_config.provision :puppet_server do |puppet_config|
+      vm_config.provision :puppet_server, run: attr(:run) do |puppet_config|
         with_attr(:puppet_server) { |val| puppet_config.puppet_server = val }
         with_attr(:node_name)     { |val| puppet_config.node_name     = val }
         with_attr(:options)       { |val| puppet_config.options       = val }
