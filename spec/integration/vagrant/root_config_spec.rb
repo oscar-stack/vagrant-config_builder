6d06af333987c266a327234636de09d034821c48
Charlie Sharpsteen <source@sharpsteen.net>
2016-02-21 15:11:13 -0800

    Add integration tests for Model::Root
    This patch adds an integration test that checks to ensure Model::Root
    delegators fire correctly by checking for the presence of things like
    settings on `config.ssh` or newly defined VMs under `config.vm.

---
+++ spec/integration/vagrant/root_config_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe 'Vagrant Integration: ConfigBuilder::Model::Root' do
+  include_context 'vagrant-unit'
+
+  # Set inside test contexts to generate Vagrant configuration.
+  let(:config_data) { Hash.new }
+
+  let(:test_env)    { isolated_environment }
+  let(:env)         { test_env.create_vagrant_env }
+  let(:root_config) { env.vagrantfile.config }
+
+  before(:each) do
+    model = ConfigBuilder::Model::Root.new_from_hash(config_data)
+
+    model.call(root_config)
+  end
+
+  context 'when configured with ssh data' do
+    let(:config_data) {
+      {'ssh' =>
+        {
+          'insert_key' => false,
+          'username'   => 'AzureDiamond',
+          'password'   => 'hunter2',
+        }
+      }
+    }
+
+    subject { root_config.ssh }
+
+    it 'sets Vagrant config.ssh parameters' do
+      expect(subject.insert_key).to be_false
+      expect(subject.username).to eq 'AzureDiamond'
+      expect(subject.password).to eq 'hunter2'
+    end
+  end
+
+  context 'when configured with WinRM data' do
+    let(:config_data) {
+      {'winrm' =>
+        {
+          'username'   => 'AzureDiamond',
+          'password'   => 'hunter2',
+        }
+      }
+    }
+
+    subject { root_config.winrm }
+
+    it 'sets Vagrant config.winrm parameters' do
+      expect(subject.username).to eq 'AzureDiamond'
+      expect(subject.password).to eq 'hunter2'
+    end
+  end
+
+  context 'when configured with NFS data' do
+    let(:config_data) {
+      {'nfs' =>
+        {
+          'map_uid'  => 42,
+        }
+      }
+    }
+
+    subject { root_config.nfs }
+
+    it 'sets Vagrant config.nfs parameters' do
+      expect(subject.map_uid).to eq 42
+    end
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
-require 'config_builder'
 
-require 'rspec'
+# Disable Vagrant autoloading so that other plugins defined in the Gemfile for
+# Acceptance tests are not loaded.
+ENV['VAGRANT_NO_PLUGINS'] = '1'
+
+require 'vagrant-spec/unit'
+require 'config_builder'
