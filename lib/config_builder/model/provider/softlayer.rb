66680585ca1ab4845ca4c4857041208a77493465
Charlie Sharpsteen <source@sharpsteen.net>
2016-02-22 12:43:46 -0800

    Update SoftLayer provider to use base class

--- lib/config_builder/model/provider/softlayer.rb
+++ lib/config_builder/model/provider/softlayer.rb
@@ -1,5 +1,5 @@
 # @see https://github.com/audiolize/vagrant-softlayer
-class ConfigBuilder::Model::Provider::SoftLayer < ConfigBuilder::Model::Base
+class ConfigBuilder::Model::Provider::SoftLayer < ConfigBuilder::Model::Provider::Base
   def_model_attribute :api_key
   def_model_attribute :endpoint_url
   def_model_attribute :username
@@ -39,62 +39,18 @@ class ConfigBuilder::Model::Provider::SoftLayer < ConfigBuilder::Model::Base
 
   def_model_delegator :load_balancers
 
-  def initialize
-    @defaults = {}
+  def instance_id
+    'softlayer'
   end
 
-  def to_proc
-    Proc.new do |vm_config|
-      vm_config.provider 'softlayer' do |config|
-        # Authentication
-        with_attr(:api_key)           { |val| config.api_key = val }
-        with_attr(:endpoint_url)      { |val| config.endpoint_url = val }
-        with_attr(:username)          { |val| config.username = val }
-        # DNS Management
-        with_attr(:manage_dns)        { |val| config.manage_dns = val }
-        # Instance Configuration
-        with_attr(:datacenter)        { |val| config.datacenter = val }
-        with_attr(:dedicated)         { |val| config.dedicated = val }
-        with_attr(:disk_capacity)     { |val| config.disk_capacity = val }
-        with_attr(:domain)            { |val| config.domain = val }
-        with_attr(:force_private_ip)  { |val| config.force_private_ip = val }
-        with_attr(:hostname)          { |val| config.hostname = val }
-        with_attr(:hourly_billing)    { |val| config.hourly_billing = val }
-        with_attr(:image_guid)        { |val| config.image_guid = val }
-        with_attr(:local_disk)        { |val| config.local_disk = val }
-        with_attr(:max_memory)        { |val| config.max_memory = val }
-        with_attr(:network_speed)     { |val| config.network_speed = val }
-        with_attr(:operating_system)  { |val| config.operating_system = val }
-        with_attr(:post_install)      { |val| config.post_install = val }
-        with_attr(:private_only)      { |val| config.private_only = val }
-        with_attr(:start_cpus)        { |val| config.start_cpus = val }
-        with_attr(:user_data)         { |val| config.user_data = val }
-        with_attr(:vlan_private)      { |val| config.vlan_private = val }
-        with_attr(:vlan_public)       { |val| config.vlan_public = val }
-        with_attr(:ssh_key)           { |val| config.ssh_key = val }
-        # Aliases for ssh_key
-        with_attr(:ssh_keys)          { |val| config.ssh_keys = val }
-        with_attr(:ssh_key_id)        { |val| config.ssh_key_id = val }
-        with_attr(:ssh_key)           { |val| config.ssh_key = val }
-        with_attr(:ssh_key_name)      { |val| config.ssh_key_name = val }
-        with_attr(:ssh_key_names)     { |val| config.ssh_key_names = val }
-        # Timeouts/Waits
-        with_attr(:api_timeout)       { |val| config.api_timeout = val }
-        with_attr(:provision_timeout) { |val| config.provision_timeout = val }
-        with_attr(:rebuild_timeout)   { |val| config.rebuild_timeout = val }
-        with_attr(:transaction_wait)  { |val| config.transaction_wait = val }
-        # Load Balancers
-        eval_models(config)
-      end
-    end
-  end
-  
   private
 
-  def eval_load_balancers(config)
+  def eval_load_balancers(configs)
+    provider, _ = configs
+
     attr(:load_balancers).each do |hash|
       v = ConfigBuilder::Model::Provider::SoftLayer::LoadBalancer.new_from_hash(hash)
-      v.call(config)
+      v.call(provider)
     end
   end
 
