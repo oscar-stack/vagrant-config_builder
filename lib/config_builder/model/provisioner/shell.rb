11a90dc307a571d31f00f84fdd2cb40bb2034593
Charlie Sharpsteen <source@sharpsteen.net>
2016-02-23 11:33:00 -0800

    Update shell provisioner to use base class
    This patch also removes the `name` attribute from the shell provisioner as it
    clashes with the `name` option that is common to all provisioners. This feature
    is purely cosmetic, so the break should be manageable.

--- lib/config_builder/model/provisioner/shell.rb
+++ lib/config_builder/model/provisioner/shell.rb
@@ -1,5 +1,5 @@
 # @see http://docs.vagrantup.com/v2/provisioning/shell.html
-class ConfigBuilder::Model::Provisioner::Shell < ConfigBuilder::Model::Base
+class ConfigBuilder::Model::Provisioner::Shell < ConfigBuilder::Model::Provisioner::Base
 
   # @!attribute [rw] inline
   #   @return [String] The inline shell command to run
@@ -33,10 +33,6 @@ class ConfigBuilder::Model::Provisioner::Shell < ConfigBuilder::Model::Base
   #   @return [Boolean] Whether Vagrant should use coloring for the output.
   def_model_attribute :keep_color
 
-  # @!attribute [rw] name
-  #   @return [String] This value will be displayed in the output.
-  def_model_attribute :name
-
   # @!attribute [rw] powershell_args
   #   @return [String] Extra arguments to pass to PowerShell if you are provisioning with PowerShell on Windows.
   def_model_attribute :powershell_args
@@ -44,24 +40,6 @@ class ConfigBuilder::Model::Provisioner::Shell < ConfigBuilder::Model::Base
   # @!attribute [rw] powershell_elevated_interactive
   #   @return [Boolean] Whether to run an elevated script in interactive mode on Windows.
   def_model_attribute :powershell_elevated_interactive
-
-  def to_proc
-    Proc.new do |vm_config|
-      vm_config.provision :shell do |shell_config|
-        with_attr(:inline)                          { |val| shell_config.inline                          = val }
-        with_attr(:path)                            { |val| shell_config.path                            = val }
-        with_attr(:args)                            { |val| shell_config.args                            = val }
-        with_attr(:env)                             { |val| shell_config.env                             = val }
-        with_attr(:binary)                          { |val| shell_config.binary                          = val }
-        with_attr(:privileged)                      { |val| shell_config.privileged                      = val }
-        with_attr(:upload_path)                     { |val| shell_config.upload_path                     = val }
-        with_attr(:keep_color)                      { |val| shell_config.keep_color                      = val }
-        with_attr(:name)                            { |val| shell_config.name                            = val }
-        with_attr(:powershell_args)                 { |val| shell_config.powershell_args                 = val }
-        with_attr(:powershell_elevated_interactive) { |val| shell_config.powershell_elevated_interactive = val }
-      end
-    end
-  end
 
   ConfigBuilder::Model::Provisioner.register('shell', self)
 end
