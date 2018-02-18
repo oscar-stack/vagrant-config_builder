7f508e96072883acbc837a6261a894ca8fa111de
Charlie Sharpsteen <chuck@puppet.com>
2018-02-18 01:05:50 -0800

    Add check for deprecated Vagrant versions
    This commit hooks an action into the Vagrant startup sequence that prints
    out a warning message if the user is running a Vagrant version for which
    we have deprecated support.

--- lib/config_builder/plugin.rb
+++ lib/config_builder/plugin.rb
@@ -10,6 +10,12 @@ module VagrantPlugins
       action_hook('ConfigBuilder: load extensions', :environment_load) do |hook|
         hook.prepend(::ConfigBuilder::Action::LoadExtensions)
       end
+
+      # Internal action hooks
+      action_hook('Vagrant Config Builder: vagrant version check', :environment_load) do |hook|
+        require 'config_builder/action/version_check'
+        hook.prepend ConfigBuilder::Action::VersionCheck
+      end
     end
   end
 end
